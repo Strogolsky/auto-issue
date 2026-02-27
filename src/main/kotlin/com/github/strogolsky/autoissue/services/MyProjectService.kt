1a1610ccf0bb4812e213492ecc025085672ce46b
Strogolsky <Strogolsky@gmail.com>
2026-02-27 10:27:19 +0100

    fix 4: code style

--- src/main/kotlin/com/github/strogolsky/autoissue/services/MyProjectService.kt
+++ src/main/kotlin/com/github/strogolsky/autoissue/services/MyProjectService.kt
@@ -1,16 +1,17 @@
 package com.github.strogolsky.autoissue.services
 
+import com.github.strogolsky.autoissue.MyBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.strogolsky.autoissue.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
-
     init {
         thisLogger().info(MyBundle.message("projectService", project.name))
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        thisLogger().warn(
+            "Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.",
+        )
     }
 
     fun getRandomNumber() = (1..100).random()
